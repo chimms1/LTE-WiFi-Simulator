cb1f4cb8be54c84d0d2d14ea761a3ea3c8d6e518
chimms1 <mail.myinbox@proton.me>
2023-01-18 16:56:47 +0530

    Multiple: Beta version, array copy is happening in place,not yet tested for multiple though

--- Simulator/rouhg.py
+++
@@ -1,8 +0,0 @@
-# from itertools import combinations_with_replacement
-
-result = list(my_combinations_with_replacement([1,2,3,4], 1))
-
-print(type(result))
-
-for i in range(0,len(result)):
-    print(result[i])
